09ac859ded6d8520290347d600ad507a99fb8db0
Jason Madigan <jason789@gmail.com>
2017-08-02 20:48:20 -0700

    Added TODO, cleaned up spacing

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+###TODO
+##Add docstring
+##Add checking for A-F only as characters
+
 import re
 
 def normalizemac(mac):
-    if (len(re.findall('\w',mac))) == 12:
-            
+    if (len(re.findall('\w',mac))) == 12:          
         result = re.sub(':','',mac)
         result = re.sub('\.','',result)
 
         print(result)  #clean
         print(':'.join(re.findall('\d\d',result)))   #colons
         print('.'.join(re.findall('\d\d\d\d',result)))   #dots
-
-   
+        
     else:
         print('Invalid MAC')
   
-normalizemac('1223.4343.4531')
